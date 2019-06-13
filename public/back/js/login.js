559302cc06c7865532f94a3c147bba04024583a7
zhangjie123123 <1846175615@qq.com>
2019-06-13 14:34:44 +0800

    登录静态页面完成，基本表单效验完成

---
+++ public/back/js/login.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2019-06-13.
+ */
+//入口函数
+$(function () {
+$("#form").bootstrapValidator({
+    //配置字段
+    fields:{
+        username:{
+            validators:{
+                //非空
+                notEmpty:{
+                    message:"用户名不能为空"
+                },
+                stringLength:{
+                    min:2,
+                    max:6,
+                    message:"用户名在2-6位数"
+                }
+            }
+        },
+        password:{
+            validators:{
+                notEmpty:{
+                    message:"密码不能为空"
+                },
+                stringLength:{
+                    min:6,
+                    max:12,
+                    message:"密码长度必须是6-12位数"
+                }
+            }
+        }
+    }
+});
+});
