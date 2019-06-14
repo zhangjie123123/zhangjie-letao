075d39b92549ab1663a1a3a6bd99c9ff86bca3d8
zhangjie123123 <1846175615@qq.com>
2019-06-14 13:40:14 +0800

    用户管理-分页渲染

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -3,19 +3,49 @@
  */
 
 $(function () {
-    $.ajax({
-        type:"get",
-        url:"/user/queryUser",
-        data:{
-            page:1,
-            pageSize:5
-        },
-        dataType:"json",
-        success:function (info) {
-            console.log(info);
-            var htmlStr = template("tpl",info);
-            $("tbody").html(htmlStr);
-        }
+    //定义全局变量
+    var currentPage = 1;
+    var pageSize=5;
 
-    });
+    render();
+    //封装ajax请求
+    function render(){
+        $.ajax({
+            type:"get",
+            url:"/user/queryUser",
+            data:{
+                page:currentPage,
+                pageSize:pageSize
+            },
+            dataType:"json",
+            success:function (info) {
+                console.log(info);
+                var htmlStr = template("tpl",info);
+                $("tbody").html(htmlStr);
+
+                //分页初始化
+                $('#paginator').bootstrapPaginator({
+                    //配置版本信息
+                    bootstrapMajorVersion:3,
+                    //指定总页数
+                    totalPages:Math.ceil(info.total/info.size),
+                    //当前显示在多少页
+                    currentPage:info.page,
+                    //当页码被点击时候调用的回调函数
+                    onPageClicked:function (a,b,c,page) {
+                        // console.log("页码被点击了")
+                        //更新当前页
+                        currentPage =page;
+                        //重新渲染
+                        render();
+                    }
+
+                });
+            }
+
+        });
+
+    }
+
+
 });
