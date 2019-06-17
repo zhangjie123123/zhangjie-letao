41a49577b945f93c2d645caad8c6562c8fa09265
zhangjie123123 <1846175615@qq.com>
2019-06-17 12:46:28 +0800

    商品列表页完成

--- public/back/js/product.js
+++ public/back/js/product.js
@@ -4,6 +4,8 @@
 $(function () {
    var currentPage = 1;//当前页
     var pageSize=5;//每页条数
+    //定义全局变量
+    var picArr=[];
     //1。一旦进入页面渲染
     render();
     function render() {
@@ -101,5 +103,160 @@ $(function () {
         //设置隐藏域
         var id =$(this).data("id");
         $('[name = "brandId"]').val(id);
+        //重置效验状态为VALID
+        $('#form').data("bootstrapValidator").updateStatus("brandId","VALID");
     });
+
+    //4.文件上传初始化
+    $("#fileupload").fileupload({
+        //返回数据格式
+        dataType:"json",
+        //文件上传完成时调用回调函数
+        done:function (e,data) {
+            console.log(data.result);
+            picArr.unshift(data.result);
+            console.log(picArr);
+            $("#imgBox").prepend('<img src="'+data.result.picAddr+'" width="100" >');
+            if(picArr.length>3){
+                //移除最后一个
+                picArr.pop();
+                $("#imgBox img").eq(-1).remove();
+            }
+            //处理后可以提交图片
+            if(picArr.length === 3){
+                $("#form").data("bootstrapValidator").updateStatus("picStatus","VALID");
+            }
+        }
+    });
+
+    //5.进行表单效验初始化
+    $("#form").bootstrapValidator({
+    //   重置排除项
+        excluded:[],
+        //配置效验图标
+        //配置效验图标
+        feedbackIcons:{
+            valid:"glyphicon glyphicon-ok",
+            invalid:"glyphicon glyphicon-remove",
+            validating:"glyphicon glyphicon-refresh"
+        },
+        //配置效验字段
+        fields:{
+            //选择二级分类
+            brandId:{
+                proName:{
+                    validators:{
+                        notEmpty:{
+                            message:"请选择二级分类"
+                        }
+                    }
+                }
+            },
+            //产品名称
+            proName:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入商品名称"
+                    }
+                }
+            },
+            //产品描述
+            proDesc:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入商品描述"
+                    }
+                }
+            },
+            //产品库存
+            //除了非空之外，要求必须是非零开头的数字
+            num:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入商品库存"
+                    },
+                    //正则效验
+                    regexp:{
+                        regexp:/^[1-9]\d*$/,
+                        message:'商品库存必须是非零开头的数字'
+                    }
+                }
+            },
+            //产品库存
+            size:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入商品尺寸"
+                    },
+                    //正则效验
+                    regexp:{
+                        regexp:/^\d{2}-\d{2}$/,
+                        message:'尺码必须是xx-xx的格式，例如32-40'
+                    }
+                }
+            },
+            //产品原价
+            oldPrice:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入商品原价"
+                    }
+                }
+            },
+            //产品现价
+            price:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入商品现价"
+                    }
+                }
+            },
+            //图片效验
+            picStatus:{
+                validators:{
+                    notEmpty:{
+                        message:"请选择三张图片"
+                    }
+                }
+
+            }
+        }
+    });
+
+    //6.阻止默认提交
+    $("#form").on("success.form.bv",function (e) {
+        //阻止默认的提交
+        e.preventDefault();
+        //声明数组
+        //获取表单的序列化数据
+        var paramsStr = $("#form").serialize();
+
+        paramsStr+="&picName1="+picArr[0].picName+"&picAddr1="+picArr[0].picAddr;
+        paramsStr+="&picName2="+picArr[1].picName+"&picAddr2="+picArr[1].picAddr;
+        paramsStr+="&picName3="+picArr[2].picName+"&picAddr3="+picArr[2].picAddr;
+        // console.log(paramsStr);
+        $.ajax({
+            type:"post",
+            url:"/product/addProduct",
+            data:paramsStr,
+            dataType:"json",
+            success:function (info) {
+                console.log(info);
+                if(info.success){
+                    //1,关闭模态框
+                    $('#addModal').modal('hide');
+                    //2.页面重新渲染
+                    currentPage = 1;
+                    render();
+                    //重置表单的内容和效验
+                    $('#form').data("bootstrapValidator").resetForm(true);
+                    //下拉列表和表单列表需要手动重置
+                    $('#dropdownText').text("请选择二级分类");
+                    $("#imgBox img").remove();//移除所有的图片
+                }
+            }
+
+        })
+    });
+
 });
